b96f338ae013edb995f1e1be4de65b40322bd19b
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-13 23:37:34 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ();
+
+INSERT INTO subgenres (name) VALUES ();
+
+INSERT INTO authors (name) VALUES ();
+
+INSERT INTO books (title, year, series_id) VALUES ();
+
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ();
+
+INSERT INTO character_books (character_id, book_id) VALUES ();
