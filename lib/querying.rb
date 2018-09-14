2d4e1e8ad7225614723c7ac2b8f8477a6c39fb42
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-14 00:49:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,47 +1,3 @@
-# CREATE TABLE series (
-#   id INTEGER PRIMARY KEY,
-#   title TEXT,
-#   author_id INTEGER,
-#   subgenre_id INTEGER
-# );
-def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series 
-  JOIN characters ON series.id=characters.series_id
-  WHERE character.species='human' ;"
-end
-#
-# CREATE TABLE subgenres (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT
-# );
-#
-# CREATE TABLE authors (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT
-# );
-#
-# CREATE TABLE books (
-#   id INTEGER PRIMARY KEY,
-#   title TEXT,
-#   year INTEGER,
-#   series_id INTEGER
-# );
-#
-# CREATE TABLE characters (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT,
-#   motto TEXT,
-#   species TEXT,
-#   author_id INTEGER,
-#   series_id INTEGER
-# );
-#
-# CREATE TABLE character_books (
-#   id INTEGER PRIMARY KEY,
-#   character_id INTEGER,
-#   book_id INTEGER
-# );
-
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year
   FROM books
@@ -72,7 +28,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  JOIN characters ON series.id=characters.series_id
+  WHERE characters.species='human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species='human') DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
