13c96c1aa626bd845e22f5a3064235a82160563e
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-14 01:00:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters
+INNER JOIN character_books ON character_books.character_id=characters.id
+GROUP BY characters.id
+ORDER BY COUNT(character_books.book_id) DESC ;"
 end
