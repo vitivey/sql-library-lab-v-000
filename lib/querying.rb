bded1735850e244185ed9450c8335963a8411e52
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-14 00:12:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -52,9 +52,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) 
-  FROM characters 
-  GROUP BY species 
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
   ORDER BY COUNT(species) DESC
   LIMIT 1;"
 end
