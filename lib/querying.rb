56b238100a4046c43fb918d8034ce6d5205344d3
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-14 00:18:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,6 +4,7 @@
 #   author_id INTEGER,
 #   subgenre_id INTEGER
 # );
+
 #
 # CREATE TABLE subgenres (
 #   id INTEGER PRIMARY KEY,
@@ -60,7 +61,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series ON series.author_id=authors.id
+  JOIN subgenres ON series.subgenre_id=subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
